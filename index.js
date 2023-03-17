dacce13735deaa8dd5d6b248784bae83defd8990
messam10 <memaq1998.10.7@gmail.com>
2023-03-18 00:49:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    }
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            let p = document.createElement("p");
+            p.textContent = json[`id`];
+            inputForm.appendChild(p);
+        })
+        .catch(function (error) {
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            inputForm.appendChild(h3);
+            console.log(error.message);
+        });
+}
+
+let inputForm = document.querySelector("form");
+
+inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    console.log(name, email);
+    submitData(name, email);
+});
